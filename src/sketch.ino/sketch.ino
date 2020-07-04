8d9b9ab0d46f8ed33551f77b34e868a4f02cce0e
Janaka A <contact@janaka.co.uk>
2020-07-04 17:59:01 +0100

    Improvements - Only refresh display when the minute rolls. - Sync from Internet time once per day.

--- src/sketch.ino/sketch.ino
+++ src/sketch.ino/sketch.ino
@@ -1,10 +1,8 @@
 #include <SPI.h>
 #include <WiFi.h>
 #include "time.h"
-//#include <WiFiUdp.h>
 
 #include "LedMatrix.h" // https://github.com/janaka/esp32-led-matrix
-#include <NTPClient.h> // https://github.com/taranais/NTPClient
 
 // to configure wifi follow instruction in `1.wifi_config.tmp`
 
@@ -16,16 +14,7 @@
 
 LedMatrix ledMatrix = LedMatrix(NUMBER_OF_DEVICES, CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
 
-const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 0;        //adjust this to you timezone
-const int daylightOffset_sec = 3600; // +1hr
-int currentMin = 60;
-
-//WiFiUDP ntpUDP;
-
-// By default 'pool.ntp.org' is used with 60 seconds update interval and
-// no offset
-//NTPClient timeClient(ntpUDP);
+unsigned int currentMin = 60;
 
 void setup()
 {
@@ -33,22 +22,12 @@ void setup()
   delay(5000); // wait sometime for the serial port to open.
   Serial.println("Boot start...");
 
-  wifiConnect();
-
-  //init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  Serial.println("Set time from internet time server.");
-  printLocalTime();
-
-  wifiDisconnect();
-  Serial.println("Disconenct Wifi.");
+  syncInternetTime();
 
   ledMatrix.init();
   ledMatrix.setIntensity(1); // range is 0-15
   ledMatrix.setCharWidth(8);
   ledMatrix.setTextAlignment(TEXT_ALIGN_LEFT);
-
-  //ledMatrix.setText("0000");
 }
 
 void loop()
@@ -67,17 +46,31 @@ void loop()
   strftime(strTime, sizeof strTime, "%H%M", &timeinfo);
   strftime(strHr, sizeof strHr, "%H", &timeinfo);
   strftime(strMin, sizeof strMin, "%M", &timeinfo);
-  strftime(strSec, sizeof strSec, "%S", &timeinfo);
+  // strftime(strSec, sizeof strSec, "%S", &timeinfo);
   Serial.println(strTime);
 
-  int thisMin = (int)strMin;
-  if (currentMin!=thisMin)
-  {
-    currentMin=thisMin;
+  unsigned int thisHr = (unsigned int)timeinfo.tm_hour;
+  unsigned int thisMin = (unsigned int)timeinfo.tm_min;
+  unsigned int thisSec = (unsigned int)timeinfo.tm_sec;
+  
+  Serial.println(thisSec);
+  
+  // Avoid drift. Sync time with the internet server once per day.
+  if ((thisHr==23) && (thisMin==59)) { 
+    syncInternetTime();
+  }
+
+  Serial.println(thisMin);
+  Serial.println(currentMin);
+
+  // Only refresh display when the minute changes.
+  if (thisMin!=currentMin) {
+    currentMin = (unsigned int)timeinfo.tm_min;
     ledMatrix.setText(strTime);
     ledMatrix.clear();
     ledMatrix.drawText();
     ledMatrix.commit();
+    Serial.println("Time refreshed on display.");
   }
 
   printLocalTime();
@@ -93,11 +86,28 @@ void printLocalTime()
     return;
   }
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.println((unsigned int)&timeinfo.tm_sec);
+  //Serial.println((unsigned int)&timeinfo.tm_sec);
 
   //tm_min
 
   //tm_hour
+}
+
+void syncInternetTime()
+{
+  const char *ntpServer = "pool.ntp.org";
+  const long gmtOffset_sec = 0;        //adjust this to you timezone
+  const int daylightOffset_sec = 3600; // +1hr
+
+  wifiConnect();
+
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.println("Set time from internet time server.");
+  printLocalTime();
+
+  wifiDisconnect();
+  
 }
 
 void wifiConnect()
@@ -137,4 +147,5 @@ void wifiDisconnect()
 {
   WiFi.disconnect(true);
   WiFi.mode(WIFI_OFF);
+  Serial.println("Disconenct Wifi.");
 }
